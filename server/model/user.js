864cc3bfed71c2d1e82782cdd91acebc1c5001ab
du <649237053@qq.com>
2019-08-14 15:19:25 +0800

    增加字段

--- server/model/user.js
+++ server/model/user.js
@@ -2,40 +2,15 @@ var Sequelize = require('sequelize')
 var sequelize = require('../mysql')
 var utils = require('../utils')
 var User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    unique: true,
-    primaryKey: true,
-    defaultValue: utils.makeuid()
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  mobile: {
-    type: Sequelize.INTEGER,
-    allowNull: true
-  },
-  LV: {
-    type: Sequelize.ENUM,
-    values: ['lv1', 'lv2']
-  },
-  createdAt: {
-    type: Sequelize.DATE,
-    defaultvalue: Date.now()
-  },
-  updatedAt: {
-    type: Sequelize.DATE,
-    defaultvalue: Date.now()
-  }
-
-}, {
-  timestamps: false
-})
+  id: { type: Sequelize.INTEGER, allowNull: false, unique: true, primaryKey: true, defaultValue: utils.makeuid() },
+  name: { type: Sequelize.STRING, allowNull: false },
+  password: { type: Sequelize.STRING, allowNull: false },
+  mobile: { type: Sequelize.INTEGER, allowNull: true },
+  LV: { type: Sequelize.ENUM, values: ['lv1', 'lv2'] },
+  createdAt: { type: Sequelize.DATE, defaultvalue: Date.now() },
+  updatedAt: { type: Sequelize.DATE, defaultvalue: Date.now() }
+},
+{ timestamps: false }
+)
 User.sync([{ force: true }])
 module.exports = { User }
