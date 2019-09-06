befda58a0a0a465f2055c111388f3c826e1ad4f2
du <649237053@qq.com>
2019-09-06 18:17:22 +0800

    qrcode

--- server/model/fulltime.js
+++ server/model/fulltime.js
@@ -3,6 +3,7 @@ var Sequelize = require('sequelize')
 /* eslint-disable no-new */
 var Fulltime = db.define('fulltime', {
   item_id: { type: Sequelize.INTEGER, primaryKey: true, allowNull: false, autoIncreament: true }, // 职位id
+  _id: { type: Sequelize.UUIDV4, allowNull: false },
   item_title: { type: Sequelize.TEXT, allowNull: false }, // 职位名称
   item_des: { type: Sequelize.STRING, allowNull: true }, // 职位描述
   item_privence: { type: Sequelize.STRING, allowNull: true }, // 归属省
@@ -18,7 +19,7 @@ var Fulltime = db.define('fulltime', {
   item_charge: { type: Sequelize.STRING, allowNull: false }, // 发布人职位
   item_position_state: {
     type: Sequelize.BOOLEAN, allowNull: false, get () { return this.getDataValue('item_position_state') ? '全职' : '兼职' }
-  }// 1 全职 0 兼职
+  }// true 全职 false 兼职
 })
 Fulltime.sync([{ force: true }]).then(res => {
   console.log('fulltime creat success !!!')
