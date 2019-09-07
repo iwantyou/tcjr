5fc3018ddef3deed684b74fb4b29604c514b1e1a
du <649237053@qq.com>
2019-09-07 15:25:11 +0800

    fix

--- src/pages/jobFair/jobFair.vue
+++ src/pages/jobFair/jobFair.vue
@@ -34,20 +34,5 @@ export default {
   }
 };
 </script>
-<<<<<<< HEAD
-<style scoped>
-.wrap-header{
-  float:left;
-  margin-right:-10px;
-}
-.wrap-content{
-  width:100px;
-  height:100px;
-  float:left;
-  background-color:#DD0B1B;
-  color:#fff;
-}
-=======
 <style lang="scss" scoped>
->>>>>>> befda58a0a0a465f2055c111388f3c826e1ad4f2
 </style>
